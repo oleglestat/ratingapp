786ea014f3e6f8a679009f8c1f6402aa6d6c7167
Oleg Chmut <oleg.lestat@gmail.com>
2017-08-15 10:48:34 +0300

    Revert "First so-so results of text recognition from image" Removing podfiles This reverts commit e923831460e0f40468e3e7dad1e2bcca7a665459.

--- WN8/WN8/PhotoSearchViewController.swift
+++ WN8/WN8/PhotoSearchViewController.swift
@@ -6,28 +6,4 @@
 //  Copyright © 2017 RoyalInn. All rights reserved.
 //
 
-import UIKit
-import TesseractOCR
-
-class PhotoSearchViewController: UIViewController, G8TesseractDelegate {
-  
-  @IBOutlet weak var textView: UITextView!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    if let tesseract = G8Tesseract(language: "eng") {
-      tesseract.delegate = self
-      tesseract.charWhitelist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_[]"
-      tesseract.pageSegmentationMode = .auto
-      tesseract.image = UIImage(named: "trainingRoom")?.g8_blackAndWhite()
-      tesseract.recognize()
-      
-      textView.text = tesseract.recognizedText
-    }
-  }
-  
-  func progressImageRecognition(for tesseract: G8Tesseract!) {
-    print("Recognition Progress \(tesseract.progress) %")
-  }
-}
+import Foundation
