20918453640b805d9339e1f606c3a419adcfd8d0
Oleg Chmut <oleg.lestat@gmail.com>
2017-08-21 21:14:33 +0300

    GPUImage 2 filtering applied

--- WN8/WN8/PhotoSearchViewController.swift
+++ WN8/WN8/PhotoSearchViewController.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2017 RoyalInn. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import GPUImage
+
+class PhotoSearchViewController: UIViewController {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var textView: UITextView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    filterImage(image: UIImage(named: "random"))
+  }
+  
+  func filterImage(image: UIImage?){
+    var endImage: UIImage!
+    if let imageToProcess = image {
+      let saturation = SaturationAdjustment()
+      saturation.saturation = 0.0
+      let trashFilter = AverageLuminanceThreshold()
+      trashFilter.thresholdMultiplier = 0.85
+      let invertFilter = ColorInversion()
+      endImage = imageToProcess.filterWithPipeline{input, output in
+        input --> saturation --> invertFilter --> trashFilter --> output
+      }
+    }
+    let pngImage = UIImagePNGRepresentation(endImage)!
+    do {
+      let documentsDir = try FileManager.default.url(for:.documentDirectory, in:.userDomainMask, appropriateFor:nil, create:true)
+      let fileURL = URL(string:"test.png", relativeTo:documentsDir)!
+      try pngImage.write(to:fileURL, options:.atomic)
+      print(documentsDir)
+    } catch {
+      print("Couldn't write to file with error: \(error)")
+    }
+  }
+}
